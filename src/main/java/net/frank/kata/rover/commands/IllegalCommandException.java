a2456f30dae2981ad8f237010ec0fd94db363d5a
Francesco Calabria <francesco.calabria@gmail.com>
2018-11-13 23:49:19 +0100

    Pulizia classe IllegalCommandException da costruttori non utilizzati

--- src/main/java/net/frank/kata/rover/commands/IllegalCommandException.java
+++ src/main/java/net/frank/kata/rover/commands/IllegalCommandException.java
@@ -1,19 +1,9 @@
 package net.frank.kata.rover.commands;
 
 public class IllegalCommandException extends Exception {
-    public IllegalCommandException() {
-        super();
-    }
 
     public IllegalCommandException(String message) {
         super(message);
     }
 
-    public IllegalCommandException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public IllegalCommandException(Throwable cause) {
-        super(cause);
-    }
 }
